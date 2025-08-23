f13876b5a45a463ee4cef7f00eaa8d143c60d765
vital345 <vitalblaze2@gmail.com>
2025-08-23 22:00:15 +0530

    feat: made it to follow cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Introduction from "@/components/Introduction";
+import { PersonModel } from "@/components/PersonModel";
+import { Canvas } from "@react-three/fiber";
 import gsap from "gsap";
 import { ScrambleTextPlugin, ScrollTrigger, SplitText } from "gsap/all";
 
@@ -8,10 +9,18 @@ gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin, SplitText);
 
 export default function Home() {
   return (
-    <div className="uppercase overflow-x-hidden">
-      <Introduction />
-      <div className="min-h-[100vh]"></div>
-      <div className="min-h-[100vh]"></div>
+    <div className="h-screen w-screen bg-gray-950">
+      <Canvas shadows camera={{ position: [0, 0, 0], fov: 30 }}>
+        {/* <div className="uppercase overflow-x-hidden">
+        <Introduction />
+        <div className="min-h-[100vh]"></div>
+        <div className="min-h-[100vh]"></div>
+      </div> */}
+        <ambientLight />
+        <pointLight position={[-0.1, -0.1, -0.03]} intensity={1} />
+        <pointLight position={[0, 0.5, -1.3]} intensity={1} />
+        <PersonModel position={[0, -1.35, -0.7]} scale={0.8} />
+      </Canvas>
     </div>
   );
 }
